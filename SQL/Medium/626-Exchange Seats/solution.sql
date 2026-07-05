7de6412d91e2f29eafd4361a8bd4845605619476
renukaA-08 <annagonirenukagoud@gmail.com>
2026-07-05 23:24:35 +0530

    create solution

--- SQL/Medium/626-Exchange Seats/solution.sql
+++ SQL/Medium/626-Exchange Seats/solution.sql
@@ -1,1 +1,11 @@
-
+select 
+    case
+        when id % 2 = 1 and  id != (select max(id) from Seat) 
+            then id+1
+        when id % 2 = 0 
+            then id-1
+        else id
+    end as id ,
+    student
+from Seat
+order by id asc
