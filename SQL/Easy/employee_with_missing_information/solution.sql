53968131531bc8b21e2d9b486c43c777504dd914
renukaA-08 <annagonirenukagoud@gmail.com>
2026-07-09 22:47:50 +0530

    Add SQL solution for employees with missing info

---
+++ SQL/Easy/employee_with_missing_information/solution.sql
@@ -0,0 +1,6 @@
+(select e.employee_id from Employees e left join Salaries s
+ON e.employee_id = s.employee_id where s.salary is null )
+union all
+(select s.employee_id from Employees e right join Salaries s 
+ON e.employee_id = s.employee_id where e.name is null )
+order by employee_id asc
