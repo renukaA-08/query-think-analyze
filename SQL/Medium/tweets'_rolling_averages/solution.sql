d3d58d06122d96cee3450e005bbbbe6608c22a84
renukaA-08 <annagonirenukagoud@gmail.com>
2026-07-13 22:52:40 +0530

    Add SQL query for 3-day rolling average of tweets

---
+++ SQL/Medium/tweets'_rolling_averages/solution.sql
@@ -0,0 +1,11 @@
+select 
+  user_id,
+  tweet_date,
+  round(
+  avg(tweet_count) over(
+    partition by user_id 
+    order by tweet_date 
+    rows between 2 preceding and current row)
+  ,2)
+  as rolling_avg_3d
+from tweets;
