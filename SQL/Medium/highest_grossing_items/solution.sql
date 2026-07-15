0f6bde23278e3a2881c4c463de574deec2f5e7f7
renukaA-08 <annagonirenukagoud@gmail.com>
2026-07-15 19:55:57 +0530

    Add SQL query for highest grossing items by category

---
+++ SQL/Medium/highest_grossing_items/solution.sql
@@ -0,0 +1,18 @@
+select 
+  category ,
+  product ,
+  total_sum
+from 
+  (
+    select 
+      category , 
+      product , 
+      sum(spend) as total_sum ,
+      row_number() over (partition by category order by sum(spend)
+       desc ) as rn
+    from
+      product_spend
+    where year(transaction_date) = 2022
+    group by category,product
+  )t
+where rn <=2
