be99ff66d24f698d0ecaad4c4e6444ea78dd9c49
renukaA-08 <annagonirenukagoud@gmail.com>
2026-08-01 19:02:55 +0530

    Create function to get Nth highest salary

---
+++ SQL/Medium/nth-highest-salary/solution.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    set N = N-1 ;
+  RETURN (
+  select 
+    distinct salary as getNthHighestSalary
+  from 
+    Employee
+  order by salary desc
+  limit 1 offset N
+  );
+END
